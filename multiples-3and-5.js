96a0e328ea058d77cf31a41fb191b7f90c8ded03
Lugonda Brian <lugondabrian@gmail.com>
2023-01-18 04:01:58 -0800

    feat:sum of multiples of 3 or 5

---
+++ multiples-3and-5.js
@@ -0,0 +1,15 @@
+
+function multiples(){
+let sum = 0;
+for(let i = 0; i<1000; i++){
+    if(i%3===0){
+        sum = sum + i;
+    }
+    else if(i%5===0){
+        sum = sum + i;
+    }
+}
+console.log(sum)
+}
+
+multiples();
